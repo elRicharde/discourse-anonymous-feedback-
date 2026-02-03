112ff309608e6f456ca37f351a8dbef6fb54564d
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-03 19:53:03 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,19 @@
+class AnonymousFeedbackController < ApplicationController
+  skip_before_action :check_xhr, only: [:index]
+  skip_before_action :preload_json, only: [:index]
+  skip_before_action :redirect_to_login_if_required, only: [:index]
+  skip_before_action :verify_authenticity_token, only: [:create]
+  
+  def index
+    # Dein Code hier
+  end
+  
+  def create
+    # Dein Code zum Senden der Nachricht
+  end
+end
+
+
 # name: discourse-anonymous-feedback
 # about: Anonymous feedback form (door code) that sends PM to a group
 # version: 0.2
@@ -10,18 +26,6 @@ after_initialize do
   
   Discourse::Application.routes.append do
     get "/anonymous-feedback" => "anonymous_feedback#index"
-  end
-  
-  # Allow this endpoint even when login is required
-  add_to_class(:application_controller, :anonymous_feedback_public) do
-    skip_before_action :check_xhr, :preload_json, :verify_authenticity_token, only: [:index]
-  end if defined?(ApplicationController)
-  
-  # Oder: Füge es direkt im Controller hinzu
-  require_dependency 'application_controller'
-  class ::AnonymousFeedbackController < ::ApplicationController
-    skip_before_action :check_xhr, only: [:index]
-    skip_before_action :preload_json, only: [:index]
-    skip_before_action :redirect_to_login_if_required, only: [:index]
+    post "/anonymous-feedback" => "anonymous_feedback#create"
   end
 end
