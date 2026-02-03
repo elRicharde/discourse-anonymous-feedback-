0e32f58598d42895629f71901535edeb5331d0a3
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-03 19:51:55 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,14 +7,21 @@ enabled_site_setting :anonymous_feedback_enabled
 
 after_initialize do
   require_dependency File.expand_path("../app/controllers/anonymous_feedback_controller.rb", __FILE__)
-
+  
   Discourse::Application.routes.append do
     get "/anonymous-feedback" => "anonymous_feedback#index"
   end
-
-  # Allow this endpoint even when SiteSetting.login_required = true
-  if defined?(::Auth::DefaultCurrentUserProvider)
-    ::Auth::DefaultCurrentUserProvider.public_routes << "anonymous_feedback#index"
+  
+  # Allow this endpoint even when login is required
+  add_to_class(:application_controller, :anonymous_feedback_public) do
+    skip_before_action :check_xhr, :preload_json, :verify_authenticity_token, only: [:index]
+  end if defined?(ApplicationController)
+  
+  # Oder: Füge es direkt im Controller hinzu
+  require_dependency 'application_controller'
+  class ::AnonymousFeedbackController < ::ApplicationController
+    skip_before_action :check_xhr, only: [:index]
+    skip_before_action :preload_json, only: [:index]
+    skip_before_action :redirect_to_login_if_required, only: [:index]
   end
 end
-
