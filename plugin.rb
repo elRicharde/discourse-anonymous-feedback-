c0f8340c8ab083c891b9ac45c7fdf9975da8138e
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-04 12:33:48 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -15,11 +15,11 @@ after_initialize do
   require_dependency File.expand_path("../app/controllers/anonymous_feedback_controller.rb", __FILE__)
 
   # Routes
-Discourse::Application.routes.append do
-  get  "/anonymous-feedback" => "anonymous_feedback#index"
-  post "/anonymous-feedback/unlock" => "anonymous_feedback#unlock"
-  post "/anonymous-feedback" => "anonymous_feedback#create"
-end
+  Discourse::Application.routes.append do
+    get  "/anonymous-feedback" => "anonymous_feedback#index"
+    post "/anonymous-feedback/unlock" => "anonymous_feedback#unlock"
+    post "/anonymous-feedback" => "anonymous_feedback#create"
+  end
 
 
   # Ensure Rails can find plugin views for this controller
